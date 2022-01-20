a71d2e2ee4897ece8d46965d38800c50ddf8efd0
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-01-20 13:20:44 -0300

    style: Set the Parser class do check if the file exist inside initialize method

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -3,13 +3,12 @@ require './lib/parser.rb'
 describe Parser do
   describe "#print_first_line" do
     it "prints the file first line" do
-      file = Parser.new('games.log')
-      #file = Parser.new('./spec/fixtures/games_test.log')
+      file = Parser.new('./spec/fixtures/games_test.log')
       expect(file.print_first_line).to eql("  0:00 ------------------------------------------------------------\n")
     end
 
     it "checks if the file exists" do
-      expect{Parser.new('game.log').print_first_line}.to raise_error(RuntimeError, "File doesn't exist")
+      expect{Parser.new('game.log')}.to raise_error(RuntimeError, "File doesn't exist")
     end
   end
 end
