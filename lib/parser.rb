663857f025931f1e231f7115a4b1d8c63d036381
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-01-20 18:20:55 -0300

    style: print_info method return simplified

--- lib/parser.rb
+++ lib/parser.rb
@@ -16,7 +16,6 @@ class Parser
   def print_info
     file = File.foreach(@file)
     file_lines = file.count
-    json_lines = {@file => {'lines' => file_lines}}.to_json
-    JSON.pretty_generate(JSON.parse(json_lines)) 
+    JSON.pretty_generate({@file => {lines: file_lines}}) 
   end
 end
