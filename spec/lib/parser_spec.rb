23c8e1a33bdbf5f2ec23698e65ca5b54469e2f1f
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-01-25 14:32:43 -0300

    feat: I have installed Rubocop and Simplecov again because I forgot to merge the branches

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,17 +1,17 @@
 require './lib/parser'
 
 describe Parser do
-  describe "#print_first_line" do
-    it "prints the file first line" do
+  describe '#print_first_line' do
+    it 'prints the file first line' do
       file = Parser.new('./spec/fixtures/games_test.log')
       expect(file.print_first_line).to eql("  0:00 ------------------------------------------------------------\n")
     end
 
-    it "checks if the file exists" do
+    it 'checks if the file exists' do
       expect { Parser.new('game.log') }.to raise_error(RuntimeError, "File doesn't exist")
     end
 
-    it "prints a json with the file location and how many lines on it" do
+    it 'prints a json with the file location and how many lines on it' do
       file = Parser.new('./spec/fixtures/games_test.log')
       expect(file.print_info).to eql("{\n  \"./spec/fixtures/games_test.log\": {\n    \"lines\": 50,\n    \"players\": [\n      \"Isgalamido\",\n      \"Dono da Bola\",\n      \"Mocinha\"\n    ]\n  }\n}")
     end
